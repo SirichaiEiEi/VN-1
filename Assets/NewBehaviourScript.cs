68d5727da8118517a68efe83ea696d5b3fcf76a4
ObeslikB <anusorn.b@gmail.com>
2022-08-30 11:05:00 +0700

    add background and character

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText.text = "?????????";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
